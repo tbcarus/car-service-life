95c4fd3b0d858d7405d70334e426b6a80a04ef5c
tbcarus <70680359+tbcarus@users.noreply.github.com>
2024-09-27 07:05:43 +0300

    Initial commit

---
+++ src/main/java/ru/tbcarus/carservicelife/CarServiceLifeApplication.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.carservicelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceLifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceLifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tbcarus/carservicelife/CarServiceLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.carservicelife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServiceLifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
